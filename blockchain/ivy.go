15cabb423df90c2e60b04f0647b33436c9210f92
oysheng <oysheng@bytom.io>
2018-03-02 10:53:15 +0800

    modify compiled contract param style

--- blockchain/ivy.go
+++ blockchain/ivy.go
@@ -41,16 +41,14 @@ func compileIvy(req compileReq) (compileResp, error) {
 		resp.Error = err.Error()
 	}
 
-	// The contract in dashboard only support single contract instance
-	contract := compiled[0]
+	// if source contract maybe contain import statement, multiple contract objects will be generated
+	// after the compilation, and the last object is what we need.
+	contract := compiled[len(compiled)-1]
 
 	resp.Name = contract.Name
 	resp.Source = req.Contract
 	resp.Value = contract.Value
-
-	for _, param := range contract.Params {
-		resp.Params = append(resp.Params, *param)
-	}
+	resp.Opcodes = contract.Opcodes
 
 	resp.Program = contract.Body
 	if req.Args != nil {
@@ -60,35 +58,39 @@ func compileIvy(req compileReq) (compileResp, error) {
 		}
 	}
 
-	for _, contract := range compiled {
-		for _, clause := range contract.Clauses {
-			info := clauseInfo{
-				Name:      clause.Name,
-				Args:      []compiler.Param{},
-				Mintimes:  clause.MinTimes,
-				Maxtimes:  clause.MaxTimes,
-				HashCalls: clause.HashCalls,
-			}
-			if info.Mintimes == nil {
-				info.Mintimes = []string{}
-			}
-			if info.Maxtimes == nil {
-				info.Maxtimes = []string{}
-			}
-
-			for _, p := range clause.Params {
-				info.Args = append(info.Args, compiler.Param{Name: p.Name, Type: p.Type})
-			}
-
-			for _, value := range clause.Values {
-				info.Values = append(info.Values, value)
-			}
-
-			resp.Clauses = append(resp.Clauses, info)
+	for _, param := range contract.Params {
+		if param.InferredType != "" {
+			param.Type = param.InferredType
+			param.InferredType = ""
 		}
+		resp.Params = append(resp.Params, *param)
 	}
 
-	resp.Opcodes = contract.Opcodes
+	for _, clause := range contract.Clauses {
+		info := clauseInfo{
+			Name:      clause.Name,
+			Args:      []compiler.Param{},
+			Mintimes:  clause.MinTimes,
+			Maxtimes:  clause.MaxTimes,
+			HashCalls: clause.HashCalls,
+		}
+		if info.Mintimes == nil {
+			info.Mintimes = []string{}
+		}
+		if info.Maxtimes == nil {
+			info.Maxtimes = []string{}
+		}
+
+		for _, p := range clause.Params {
+			info.Args = append(info.Args, compiler.Param{Name: p.Name, Type: p.Type})
+		}
+
+		for _, value := range clause.Values {
+			info.Values = append(info.Values, value)
+		}
+
+		resp.Clauses = append(resp.Clauses, info)
+	}
 
 	return resp, nil
 }
